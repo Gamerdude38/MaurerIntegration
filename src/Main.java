5b7721b570aeb6ae466c88b71850a6aa34a430a9
Gamerdude38 <43382258+Gamerdude38@users.noreply.github.com>
2019-01-29 15:09:45 -0500

    Created PlayingCard and Deck classes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 //John Maurer
 // A program integrating skills learned in COP 2006.
 
+/*Foreword: I'm thinking of making a Blackjack card game. Or perhaps a collection of card games using ASCII graphics
+This main class for the time being is serving as a way for me to at least make sure I have the minimum requirements
+complete for the integration project milestones. Hopefully by the time I actually sit down for a couple of hours to work
+on this, a lot of the code here will actually be replaced by code that will actually run the game. I'm just going to start
+with a PLayingCard class and go from there, pls don't be mad.
+*/
+
 //typing the word "sysout", then pressing [Ctrl + Space] yields "System.out.println();"
 //scan.nextLine(); consumes the rest of the line. Any time you go from entering a number to entering a string, add a "scan.nextLine();" before "String a = scan.nextLine();"
 
@@ -29,13 +36,40 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 	
 		
-		System.out.println("Welcome to this integration project!");
-		System.out.println(isTrue);
-		System.out.println(myString);
-		System.out.println(myNumber);
-		System.out.println(myDouble);
+		System.out.println("Welcome to this integration project! If you tune in long enough, it'll turn into a game of Blackjack!");
+		System.out.println("Boolean: isTrue = " + isTrue);
+		System.out.println("String: myString = " + myString);
+		System.out.println("Int: myNumber = " + myNumber);
+		System.out.println("Double: myDouble = " + myDouble);
 		
+		myString = myString.toLowerCase(); //.toLowerCase returns a string where all the letters are lower case. There's an upper case method that works similarly to this one.
+		
+		String mySubstring = myString.substring(0, 1); //.substring returns a string that is within the range of the given string. In this example, "HEya" from 0 to 1 will return "H" as a new string
+		
+		System.out.println("String: mySubstring = " + mySubstring);
+		
+		if (mySubstring.compareTo(myString) >= 1) { //.compareTo returns an integer that represents the lexicographic distance one string is from another. If mySubstring is larger than myString lexicographically, it will return a positive number. If they're the same, it will return 0. If mySubstring is smaller, it will return a negative number)
+			System.out.println("\"" + mySubstring + "\" is lexicographically bigger than \"" + myString + "\".");
+		}else if (mySubstring.compareTo(myString) == 0) {
+			System.out.println("\"" + mySubstring + "\" is lexicographically the same as \"" + myString + "\".");
+		}else {
+			System.out.println("\"" + mySubstring + "\" is lexicographically smaller than \"" + myString + "\".");
+		}
+		
+		int dubToInt = (int) myDouble; //This is an example of casting. By putting (int) before myDouble, I will be able to put the double into an integer. The 2.3 will lose its .3 and just become 2.
+		
+		System.out.println("myDouble being casted to an integer results in: " + dubToInt);
 		scan.close();
+		
+		System.out.println("Time for the real fun to begin. \n");
+		
+		Deck myDeck = new Deck();
+		
+		System.out.println("Brand new deck of cards: \n" + myDeck.toString());
+		
+		myDeck.shuffle();
+		
+		System.out.println("The same deck, but shuffled now: \n" + myDeck.toString());
 	}
 
 }
---
+++ src/PlayingCard.java
@@ -0,0 +1,61 @@
+//John Maurer
+//This class defines a PlayingCard object with a suit, a denomination, and a value (calculated via denomination)
+
+public class PlayingCard {
+	private String suit;
+	private String denomination;
+	private int value;
+	
+	PlayingCard(String denomination, String suit){
+		this.suit = suit;
+		this.denomination = denomination;
+		
+		switch(denomination) {
+			case "Ace": value = 1;
+				break;
+			case "Two": value = 2;
+				break;
+			case "Three": value = 3;
+				break;
+			case "Four": value = 4;
+				break;
+			case "Five": value = 5;
+				break;
+			case "Six": value = 6;
+				break;
+			case "Seven": value = 7;
+				break;
+			case "Eight": value = 8;
+				break;
+			case "Nine": value = 9;
+				break;
+			case "Ten": value = 10;
+				break;
+			case "Jack": value = 10;
+				break;
+			case "Queen": value = 10;
+				break;
+			case "King": value = 10;
+				break;
+			default: value = 0; suit = "INVALID";
+				break;
+		}
+		
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+	public String getDenomination() {
+		return denomination;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public String toString() {
+		return denomination + " of " + suit + ".";
+	}
+}
