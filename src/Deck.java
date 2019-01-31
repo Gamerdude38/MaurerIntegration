9cb35c49a67a41baecedc75537b7e1a452388b66
Gamerdude38 <43382258+Gamerdude38@users.noreply.github.com>
2019-01-31 15:38:23 -0500

    Applied Google Style formatting

--- src/Deck.java
+++ src/Deck.java
@@ -1,73 +1,97 @@
-//John Maurer
-//A class that uses the PlayingCard class to create a Deck object, with 52 unique cards. Also has handy methods for shuffling and stuff
+// John Maurer
+// A class that uses the PlayingCard class to create a Deck object, with 52 unique cards. Also has
+// handy methods for shuffling and stuff
 
 public class Deck {
-	private final int MAX_CARDS = 52;
-	private PlayingCard[] deck = new PlayingCard[MAX_CARDS];
-	
-	public Deck() {
-		int deckMarker = 0; //deckMarker is going to increment by 1 at the end of the denom for loop to keep the placement of the cards
-		
-		for (int suit = 0; suit < 4; suit++) {
-			for(int denom = 0; denom < 13; denom++) { //"denom" stands for card denomination
-				PlayingCard temp = new PlayingCard(this.deriveDenom(denom), this.deriveSuit(suit));
-				deck[deckMarker] = temp;
-				
-				deckMarker++;
-			}
-		}
-	}
-	
-	private String deriveSuit(int suit) {
-		switch (suit) {
-			case 0: return "Spades";
-			case 1: return "Diamonds";
-			case 2: return "Hearts";
-			case 3: return "Clubs";
-			default: return null;
-		}
-	}
-	
-	private String deriveDenom(int denom) {
-		switch (denom) {
-			case 0: return "Ace";
-			case 1: return "Two";
-			case 2: return "Three";
-			case 3: return "Four";
-			case 4: return "Five";
-			case 5: return "Six";
-			case 6: return "Seven";
-			case 7: return "Eight";
-			case 8: return "Nine";
-			case 9: return "Ten";
-			case 10: return "Jack";
-			case 11: return "Queen";
-			case 12: return "King";
-			default: return null;
-		}
-	}
-	
-	public String toString() {
-		String str = "";
-		
-		for (int i = 0; i < MAX_CARDS; i++) {
-			str += (deck[i].toString() + "\n");
-		}
-		
-		return str;
-	}
-	
-	public void shuffle() {
-		int firstIndex = 0;
-		int secondIndex = 0; //these two indices will serve as the two cards whose positions will be exchanged
-		
-		for(int i = 0; i < 500; i++) { //to ensure good shuffling, I give the random number generator a chance to shuffle every card. Its a basic shuffling method, but hopefully 200 iterations will shuffle the cards nicely
-			firstIndex = (int)(Math.random() * 52);
-			secondIndex = (int)(Math.random() * 52);
-			
-			PlayingCard temp = deck[firstIndex];
-			deck[firstIndex] = deck[secondIndex];
-			deck[secondIndex] = temp;
-		}
-	}
+  private final int MAX_CARDS = 52;
+  private PlayingCard[] deck = new PlayingCard[MAX_CARDS];
+
+  public Deck() {
+    int deckMarker = 0; // deckMarker is going to increment by 1 at the end of the denom for loop to
+                        // keep the placement of the cards
+
+    for (int suit = 0; suit < 4; suit++) {
+      for (int denom = 0; denom < 13; denom++) { // "denom" stands for card denomination
+        PlayingCard temp = new PlayingCard(this.deriveDenom(denom), this.deriveSuit(suit));
+        deck[deckMarker] = temp;
+
+        deckMarker++;
+      }
+    }
+  }
+
+  private String deriveSuit(int suit) {
+    switch (suit) {
+      case 0:
+        return "Spades";
+      case 1:
+        return "Diamonds";
+      case 2:
+        return "Hearts";
+      case 3:
+        return "Clubs";
+      default:
+        return null;
+    }
+  }
+
+  private String deriveDenom(int denom) {
+    switch (denom) {
+      case 0:
+        return "Ace";
+      case 1:
+        return "Two";
+      case 2:
+        return "Three";
+      case 3:
+        return "Four";
+      case 4:
+        return "Five";
+      case 5:
+        return "Six";
+      case 6:
+        return "Seven";
+      case 7:
+        return "Eight";
+      case 8:
+        return "Nine";
+      case 9:
+        return "Ten";
+      case 10:
+        return "Jack";
+      case 11:
+        return "Queen";
+      case 12:
+        return "King";
+      default:
+        return null;
+    }
+  }
+
+  public String toString() {
+    String str = "";
+
+    for (int i = 0; i < MAX_CARDS; i++) {
+      str += (deck[i].toString() + "\n");
+    }
+
+    return str;
+  }
+
+  public void shuffle() {
+    int firstIndex = 0;
+    int secondIndex = 0; // these two indices will serve as the two cards whose positions will be
+                         // exchanged
+
+    for (int i = 0; i < 200; i++) { // to ensure good shuffling, I give the random number generator
+                                    // a chance to shuffle every card. Its a basic shuffling method,
+                                    // but hopefully 200 iterations will shuffle the cards nicely
+      firstIndex = (int) (Math.random() * 52);
+      secondIndex = (int) (Math.random() * 52);
+
+      PlayingCard temp = deck[firstIndex];
+      deck[firstIndex] = deck[secondIndex];
+      deck[secondIndex] = temp;
+    }
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,100 @@
 import java.util.Scanner;
 
-//John Maurer
+// John Maurer
 // A program integrating skills learned in COP 2006.
 
-/*Foreword: I'm thinking of making a Blackjack card game. Or perhaps a collection of card games using ASCII graphics
-This main class for the time being is serving as a way for me to at least make sure I have the minimum requirements
-complete for the integration project milestones. Hopefully by the time I actually sit down for a couple of hours to work
-on this, a lot of the code here will actually be replaced by code that will actually run the game. I'm just going to start
-with a PLayingCard class and go from there, pls don't be mad.
-*/
+/*
+ * Foreword: I'm thinking of making a Blackjack card game. Or perhaps a collection of card games
+ * using ASCII graphics This main class for the time being is serving as a way for me to at least
+ * make sure I have the minimum requirements complete for the integration project milestones.
+ * Hopefully by the time I actually sit down for a couple of hours to work on this, a lot of the
+ * code here will actually be replaced by code that will actually run the game. I'm just going to
+ * start with a PLayingCard class and go from there, pls don't be mad.
+ */
 
-//typing the word "sysout", then pressing [Ctrl + Space] yields "System.out.println();"
-//scan.nextLine(); consumes the rest of the line. Any time you go from entering a number to entering a string, add a "scan.nextLine();" before "String a = scan.nextLine();"
+// typing the word "sysout", then pressing [Ctrl + Space] yields "System.out.println();"
+// scan.nextLine(); consumes the rest of the line. Any time you go from entering a number to
+// entering a string, add a "scan.nextLine();" before "String a = scan.nextLine();"
 
-//Variable: A place in memory to store a value
-//Scope: Describes the location in which a Variable is defined. A variable defined in a method is a "local" variable and is only accessible within that method. A variable defined in a class, but not a method, is a "global" variable and is accessible from all methods within the class.
+// Variable: A place in memory to store a value
+// Scope: Describes the location in which a Variable is defined. A variable defined in a method is a
+// "local" variable and is only accessible within that method. A variable defined in a class, but
+// not a method, is a "global" variable and is accessible from all methods within the class.
 
-//Byte: Consists of 8 bits, can represent 2^8 numbers, from -128 to 127 (integers)
-//Short: 16 bits, 2^16 (integers)
-//Int: 32 bits 2^32 (integers)
-//Long: 64 bits, 2^64 (integers) Ends with a 'L'
-//Float: 32 bit, single precision (0.1) (decimal) Ends with a 'f'
-//Double: 64 bit, double precision (0.01) (decimal) Ends with a 'd'
-//Boolean: Undefined size, holds either true or false, so it acts like a special bit
-//Char: Single 16-bit Unicode character from '\u0000' (0) to '\uffff' (65,535) Written with singular quotes, NOT double quotes
+// Byte: Consists of 8 bits, can represent 2^8 numbers, from -128 to 127 (integers)
+// Short: 16 bits, 2^16 (integers)
+// Int: 32 bits 2^32 (integers)
+// Long: 64 bits, 2^64 (integers) Ends with a 'L'
+// Float: 32 bit, single precision (0.1) (decimal) Ends with a 'f'
+// Double: 64 bit, double precision (0.01) (decimal) Ends with a 'd'
+// Boolean: Undefined size, holds either true or false, so it acts like a special bit
+// Char: Single 16-bit Unicode character from '\u0000' (0) to '\uffff' (65,535) Written with
+// singular quotes, NOT double quotes
 
 public class Main {
 
-	public static void main(String[] args) {
-		boolean isTrue = true;
-		String myString = "HEya";
-		final int myNumber = 7; //the keyword "final" means that this variable cannot be changed later. myNumber will always be the number it is assigned to on this line, regardless of the code after this line.
-		double myDouble = 2.3;
-		
-		Scanner scan = new Scanner(System.in);
-	
-		
-		System.out.println("Welcome to this integration project! If you tune in long enough, it'll turn into a game of Blackjack!");
-		System.out.println("Boolean: isTrue = " + isTrue);
-		System.out.println("String: myString = " + myString);
-		System.out.println("Int: myNumber = " + myNumber);
-		System.out.println("Double: myDouble = " + myDouble);
-		
-		myString = myString.toLowerCase(); //.toLowerCase returns a string where all the letters are lower case. There's an upper case method that works similarly to this one.
-		
-		String mySubstring = myString.substring(0, 1); //.substring returns a string that is within the range of the given string. In this example, "HEya" from 0 to 1 will return "H" as a new string
-		
-		System.out.println("String: mySubstring = " + mySubstring);
-		
-		if (mySubstring.compareTo(myString) >= 1) { //.compareTo returns an integer that represents the lexicographic distance one string is from another. If mySubstring is larger than myString lexicographically, it will return a positive number. If they're the same, it will return 0. If mySubstring is smaller, it will return a negative number)
-			System.out.println("\"" + mySubstring + "\" is lexicographically bigger than \"" + myString + "\".");
-		}else if (mySubstring.compareTo(myString) == 0) {
-			System.out.println("\"" + mySubstring + "\" is lexicographically the same as \"" + myString + "\".");
-		}else {
-			System.out.println("\"" + mySubstring + "\" is lexicographically smaller than \"" + myString + "\".");
-		}
-		
-		int dubToInt = (int) myDouble; //This is an example of casting. By putting (int) before myDouble, I will be able to put the double into an integer. The 2.3 will lose its .3 and just become 2.
-		
-		System.out.println("myDouble being casted to an integer results in: " + dubToInt);
-		scan.close();
-		
-		System.out.println("Time for the real fun to begin. \n");
-		
-		Deck myDeck = new Deck();
-		
-		System.out.println("Brand new deck of cards: \n" + myDeck.toString());
-		
-		myDeck.shuffle();
-		
-		System.out.println("The same deck, but shuffled now: \n" + myDeck.toString());
-	}
+  public static void main(String[] args) {
+    boolean isTrue = true;
+    String myString = "HEya";
+    final int myNumber = 7; // the keyword "final" means that this variable cannot be changed later.
+                            // myNumber will always be the number it is assigned to on this line,
+                            // regardless of the code after this line.
+    double myDouble = 2.3;
+
+    Scanner scan = new Scanner(System.in);
+
+
+    System.out.println(
+        "Welcome to this integration project! If you tune in long enough, it'll turn into a game of Blackjack!");
+    System.out.println("Boolean: isTrue = " + isTrue);
+    System.out.println("String: myString = " + myString);
+    System.out.println("Int: myNumber = " + myNumber);
+    System.out.println("Double: myDouble = " + myDouble);
+
+    myString = myString.toLowerCase(); // .toLowerCase returns a string where all the letters are
+                                       // lower case. There's an upper case method that works
+                                       // similarly to this one.
+
+    String mySubstring = myString.substring(0, 1); // .substring returns a string that is within the
+                                                   // range of the given string. In this example,
+                                                   // "HEya" from 0 to 1 will return "H" as a new
+                                                   // string
+
+    System.out.println("String: mySubstring = " + mySubstring);
+
+    if (mySubstring.compareTo(myString) >= 1) { // .compareTo returns an integer that represents the
+                                                // lexicographic distance one string is from
+                                                // another. If mySubstring is larger than myString
+                                                // lexicographically, it will return a positive
+                                                // number. If they're the same, it will return 0. If
+                                                // mySubstring is smaller, it will return a negative
+                                                // number)
+      System.out.println(
+          "\"" + mySubstring + "\" is lexicographically bigger than \"" + myString + "\".");
+    } else if (mySubstring.compareTo(myString) == 0) {
+      System.out.println(
+          "\"" + mySubstring + "\" is lexicographically the same as \"" + myString + "\".");
+    } else {
+      System.out.println(
+          "\"" + mySubstring + "\" is lexicographically smaller than \"" + myString + "\".");
+    }
+
+    int dubToInt = (int) myDouble; // This is an example of casting. By putting (int) before
+                                   // myDouble, I will be able to put the double into an integer.
+                                   // The 2.3 will lose its .3 and just become 2.
+
+    System.out.println("myDouble being casted to an integer results in: " + dubToInt);
+    scan.close();
+
+    System.out.println("Time for the real fun to begin. \n");
+
+    Deck myDeck = new Deck();
+
+    System.out.println("Brand new deck of cards: \n" + myDeck.toString());
+
+    myDeck.shuffle();
+
+    System.out.println("The same deck, but shuffled now: \n" + myDeck.toString());
+  }
 
 }
